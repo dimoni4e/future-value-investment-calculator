afe7f6606d2185a27f186eb7e52c652227b7fae2
dimoni4e <54023446+dimoni4e@users.noreply.github.com>
2025-06-09 16:27:25 +0200

    Test commit with bad code to verify pre-commit hook

--- test-bad-code.js
+++ test-bad-code.js
@@ -1,3 +1,14 @@
-// Test file with bad formatting
-const badCode = 'test'
-console.log(badCode)
+// This file contains intentionally bad code to test pre-commit hooks
+const badCode = function () {
+  var x = 1 // Should use const/let instead of var
+  console.log('missing semicolon')
+  return x
+}
+
+// Unused variable
+const unused = 'this variable is never used'
+
+// Bad formatting
+if (true) {
+  console.log('bad formatting')
+}
